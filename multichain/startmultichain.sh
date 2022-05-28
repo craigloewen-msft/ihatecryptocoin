514175aa5b6f21855365519efa1160a9708a7536
Craig Loewen <crloewen@microsoft.com>
2022-05-28 23:24:22 +0000

    Fixed prod

--- multichain/startmultichain.sh
+++ multichain/startmultichain.sh
@@ -7,7 +7,7 @@ fi
 
 if [[ ! -z "${MULTICHAIN_CONNECT_ADDRESS}" ]]; then
     echo "Connecting to server remotely";
-    ./multichaind $MULTICHAIN_CONNECT_ADDRESS -datadir=./.multichain
+    ./multichaind $MULTICHAIN_CONNECT_ADDRESS
 else
     echo "Running server directly";
     ./multichaind ihatecryptocoin -datadir=./.multichain
