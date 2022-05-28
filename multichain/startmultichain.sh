edf4f1feeade4db952aba5812dcd83b0d0a771c4
Craig Loewen <crloewen@microsoft.com>
2022-05-28 23:36:00 +0000

    Updated prod

--- multichain/startmultichain.sh
+++ multichain/startmultichain.sh
@@ -7,7 +7,7 @@ fi
 
 if [[ ! -z "${MULTICHAIN_CONNECT_ADDRESS}" ]]; then
     echo "Connecting to server remotely";
-    ./multichaind $MULTICHAIN_CONNECT_ADDRESS
+    ./multichaind $MULTICHAIN_CONNECT_ADDRESS -server=1 -rpcbind=127.0.0.1 -rpcuser=multichainrpc -rpcpassword=devpassword
 else
     echo "Running server directly";
     ./multichaind ihatecryptocoin -datadir=./.multichain
