d1bfbd2d6d450bd9b0a63c9b6e0d194d8ac1a7f3
Craig Loewen <crloewen@microsoft.com>
2022-05-29 03:25:22 +0000

    Updated prod

--- backend/defaultconfig.js
+++ backend/defaultconfig.js
@@ -9,4 +9,5 @@ module.exports = {
     'adminMultichainAddress': '1EDLGniBpv58zYsyKhkKUEiyJH74aKjKNZ7qsC',
     'adminMultichainPrivKey': 'V7KmHcwWSUoAFpKpb4FFyWAgUgXdMeSF3syQSBWpBt22uhtExekVv5tB'
     // MULTICHAIN_CONNECT_ADDRESS environment variable needs to be set to connect to an external node in production scenarios. e.g: mycryptocoin@123.0.134.1:2765
+    // 'MULTICHAIN_CONNECT_PORT also needs to be set as an environment variable '2765',
 };
